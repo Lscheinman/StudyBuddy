1dc2c5e12d36dd744a5fafecce8e961dd9811103
Lynn Scheinman <lscheinman77@gmail.com>
2024-12-04 16:41:25 -0500

    Css troubleshooting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import 'react-toastify/dist/ReactToastify.css'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
